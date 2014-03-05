5e1ad77d9f9f20e096656a6bb4c1310d80655fc9
andrewinfosec <andrewinfosec@gmail.com>
2014-03-04 16:10:20 -0800

    first commit

---
+++ README.markdown
@@ -0,0 +1,48 @@
+
+### markdown-ize: Convert Markdown files into a portable HTML+CSS package
+
+By Andrew Stewart ([http://andrewinfosec.com](http://andrewinfosec.com))
+
+[markdown-ize](https://github.com/andrewinfosec/markdown-ize) is a Bash script
+that creates a directory containing HTML files from a source directory
+containing Markdown files. The file names of the Markdown files are used to
+generate an `index.html` and a style sheet is also placed in the destination
+directory for portability.
+
+An example use-case is taking notes in plaintext Markdown across several files,
+then processing those files so that they can be moved as a single unit onto a
+web server.
+
+#### Example
+
+    $ ls 
+    README.markdown default.css md  markdown-ize.sh
+    $ ls md
+    one.md    three.md  two.md
+    $ ./markdown-ize.sh ./md/*.md
+    ...
+    $ ls html
+    default.css index.html  one.html  three.html  two.html
+    $ open html/index.html
+
+#### Options
+
+    $ ./markdown-ize.sh -h
+    usage: markdown-ize.sh [options] <markdown files>
+
+    options: -d <destination directory>  # defaults to ./html
+             -c <css file>               # defaults to ./default.css
+
+    e.g. $ markdown-ize.sh *.md
+
+
+#### Enhancements
+
+A possible future enhancement would be to interrogate file timestamps so that
+only those Markdown files that have changed since the last run are processed.
+
+#### Style
+
+This project follows the ([Google Shell Style
+Guide](http://google-styleguide.googlecode.com/svn/trunk/shell.xml)).
+
---
+++ markdown-ize.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# Convert Markdown files into a portable HTML+CSS package
+
+function usage() {
+  local PROGRAM=$(basename $0)
+  cat << HERE
+usage: $PROGRAM [options] <markdown files>
+
+options: -d <destination directory>  # defaults to ./html
+         -c <css file>               # defaults to ./default.css
+
+e.g. $ $PROGRAM *.md
+
+HERE
+}
+
+DESTINATION_DIR=./html
+CSS=./default.css
+
+while getopts hs:d:c: ARG; do
+  case $ARG in
+    h|-help)
+      usage 
+      exit 0
+      ;;
+    d)
+      DESTINATION_DIR=$OPTARG
+      ;;
+    c)
+      CSS=$OPTARG
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+SOURCE_FILES=$@
+
+readonly DESTINATION_DIR CSS SOURCE_FILES
+
+# Sanity-check environment
+#
+if [[ -z "$SOURCE_FILES" ]]; then
+  echo "error: no files to process" >&2
+  exit 1
+fi
+
+if [[ ! -d $DESTINATION_DIR ]]; then
+  mkdir $DESTINATION_DIR || { exit 1; }
+fi
+
+if [[ ! -w $DESTINATION_DIR ]]; then
+  chmod u+rwx $DESTINATION_DIR || { exit 1; }
+fi
+
+if [[ ! -r $CSS ]]; then
+  echo "error: can't read $CSS" >&2
+  exit 1
+fi
+
+if ! hash Markdown.pl 2>/dev/null; then # is Markdown.pl in the path?
+  echo "error: no Markdown.pl" >&2
+  exit 1
+else
+  MARKDOWN=Markdown.pl
+fi
+
+# Create package = HTML files + HTML index + CSS file
+#
+cp $CSS $DESTINATION_DIR || { exit 1; }
+
+INDEX_TEXT=
+
+for path_and_file in $SOURCE_FILES; do
+  echo "<link href=\"$(basename $CSS)\" rel=\"stylesheet\"></link>" > $$
+  $MARKDOWN --html4tags $path_and_file >> $$
+
+  FILENAME=$(basename $path_and_file)
+  mv $$ $DESTINATION_DIR/${FILENAME%\.*}.html
+
+  echo -n '.'
+
+  NAME=${FILENAME%\.*}
+  INDEX_TEXT=$INDEX_TEXT"[$NAME](./$NAME.html)  \n"
+  # (because a double space creates a newline in Markdown)
+done
+echo # create a newline after the earlier use of echo -n
+
+echo "<link href=\"$(basename $CSS)\" rel=\"stylesheet\"></link>" > $$
+
+IFS='%' # preserve whitespace to preserve double spaces in Markdown
+echo -e $INDEX_TEXT >> $$
+unset IFS
+
+$MARKDOWN --html4tags $$ > $DESTINATION_DIR/index.html
+rm $$
+
+# EOF
